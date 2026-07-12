f7a428b5a5b8e91616bad14294eec3ed8f2f59ab
mrdarkking632 <Adeel787403@gmail.com>
2026-07-12 21:17:52 +0500

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  PREFIX: ".",
+  BOT_NAME: "Adeel-MD",
+  OWNER_NAME: "Adeel",
+  OWNER_NUMBER: "923288835468",
+  VERSION: "3.0.0",
+  HEROKU: true,
+  FOOTER: "Powered By Adeel-MD"
+};
