913dc46b90f3454ea815aa96074e4156831ac965
mrdarkking632 <Adeel787403@gmail.com>
2026-07-13 17:06:17 +0500

    Update message.js

--- handlers/message.js
+++ handlers/message.js
@@ -1,5 +1,32 @@
+const { commands } = require("./command");
+
 module.exports = {
     handleMessage: async (sock, msg) => {
-        console.log("📩 Message Received");
+
+        if (!msg.message) return;
+
+        const text =
+            msg.message.conversation ||
+            msg.message.extendedTextMessage?.text ||
+            "";
+
+        if (!text.startsWith(".")) return;
+
+        const args = text
+            .slice(1)
+            .trim()
+            .split(/ +/);
+
+        const commandName = args.shift().toLowerCase();
+
+        const command = commands.get(commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(sock, msg, args);
+        } catch (err) {
+            console.log("❌ Command Error:", err);
+        }
     }
 };
