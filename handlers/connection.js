f38d9cd8eae292cc6abc8a938a55d4560e54c4a4
mrdarkking632 <Adeel787403@gmail.com>
2026-07-13 15:30:18 +0500

    Update connection.js

--- handlers/connection.js
+++ handlers/connection.js
@@ -1,14 +1,1 @@
-if (connection === "close") {
-    console.log("Disconnect reason:", lastDisconnect);
 
-    const statusCode = lastDisconnect?.error?.output?.statusCode;
-    console.log("Status Code:", statusCode);
-
-    if (statusCode === DisconnectReason.loggedOut) {
-        console.log("🚪 Logged Out");
-        return;
-    }
-
-    console.log("❌ Connection Closed. Reconnecting...");
-    setTimeout(() => startBot(), 3000);
-}
