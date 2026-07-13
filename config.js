f55e661f647fd962e96c1c4b8c7d54de1d707853
mrdarkking632 <Adeel787403@gmail.com>
2026-07-13 19:08:57 +0500

    Update config.js with new version and additional fields

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  PREFIX: ".",
-  BOT_NAME: "Adeel-MD",
-  OWNER_NAME: "Adeel",
-  OWNER_NUMBER: "923288835468",
-  VERSION: "3.0.0",
-  HEROKU: true,
-  FOOTER: "Powered By Adeel-MD"
+    BOT_NAME: "Adeel-MD",
+    OWNER_NAME: "Adeel",
+    OWNER_NUMBER: "923288835468",
+    PREFIX: ".",
+    VERSION: "3.2.0",
+    PACKNAME: "Adeel-MD",
+    AUTHOR: "Adeel",
+    MODE: "public"
 };
