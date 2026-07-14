85fe85fff2aadd24ac3c207e445e4d1b59b31e4d
mrdarkking632 <Adeel787403@gmail.com>
2026-07-14 15:21:31 +0500

    Update tiktok.js

--- commands/download/tiktok.js
+++ commands/download/tiktok.js
@@ -1,16 +1,35 @@
+const { ttdl } = require("btch-downloader");
+
 module.exports = {
     name: "tiktok",
 
-    async execute(sock, msg) {
-        await sock.sendMessage(msg.key.remoteJid, {
-            text: `
-📱 TikTok Downloader
+    async execute(sock, msg, args) {
+        const url = args.join(" ");
 
-Use:
-.tiktok link
+        if (!url) {
+            return await sock.sendMessage(msg.key.remoteJid, {
+                text: "❌ Example:\n.tiktok https://vt.tiktok.com/xxxxx"
+            });
+        }
 
-⏳ TikTok system connecting...
-`
-        });
+        try {
+            await sock.sendMessage(msg.key.remoteJid, {
+                text: "⏳ Downloading TikTok..."
+            });
+
+            const data = await ttdl(url);
+
+            await sock.sendMessage(msg.key.remoteJid, {
+                video: { url: data.video },
+                caption: `✅ TikTok Downloaded\n\n👑 Adeel-MD`
+            });
+
+        } catch (e) {
+            console.log(e);
+
+            await sock.sendMessage(msg.key.remoteJid, {
+                text: "❌ Download Failed."
+            });
+        }
     }
 };
