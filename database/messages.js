a6b5449e1ef40ea5d37e335aa2635e3f0375cda4
mrdarkking632 <Adeel787403@gmail.com>
2026-07-14 16:21:46 +0500

    Refactor message storage to use messages Map
    Replaced store with messages Map for message handling and added memory limit.

--- database/messages.js
+++ database/messages.js
@@ -1,11 +1,17 @@
-const store = new Map();
+const messages = new Map();
 
 function save(id, message) {
-    store.set(id, message);
+    messages.set(id, message);
+
+    // limit (memory safe)
+    if (messages.size > 500) {
+        const firstKey = messages.keys().next().value;
+        messages.delete(firstKey);
+    }
 }
 
 function get(id) {
-    return store.get(id);
+    return messages.get(id);
 }
 
 module.exports = {
