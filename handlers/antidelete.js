677cf12f91007f6b00e4b5fcb6f3b91ce27d6b40
mrdarkking632 <Adeel787403@gmail.com>
2026-07-16 13:44:14 +0500

    Update antidelete.js

--- handlers/antidelete.js
+++ handlers/antidelete.js
@@ -1,11 +1,19 @@
 const messageDB = require("../database/messages");
 
+const OWNER = "923288835468@s.whatsapp.net";
+
 async function antiDelete(sock, updates) {
     for (const update of updates) {
+
         if (!update.update?.message) continue;
 
         const msg = messageDB.get(update.key.id);
         if (!msg) continue;
+
+        const sender = update.key.participant || update.key.remoteJid;
+
+        // Ignore owner's deleted messages
+        if (sender === OWNER) continue;
 
         const text =
             msg.message?.conversation ||
@@ -13,8 +21,14 @@ async function antiDelete(sock, updates) {
             "📷 Media Message";
 
         await sock.sendMessage(update.key.remoteJid, {
-            text: `🚫 *ANTI DELETE*\n\n👤 User: @${(update.key.participant || update.key.remoteJid).split("@")[0]}\n\n📝 Deleted Message:\n${text}`,
-            mentions: [update.key.participant || update.key.remoteJid]
+            text:
+`🚫 *ANTI DELETE*
+
+👤 User: @${sender.split("@")[0]}
+
+📝 Deleted Message:
+${text}`,
+            mentions: [sender]
         });
     }
 }
