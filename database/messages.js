5114c198dbc4c17bad3f360ebe340065a82c26aa
mrdarkking632 <Adeel787403@gmail.com>
2026-07-16 15:35:08 +0500

    Refactor message handling in messages.js
    Refactor save and get functions to simplify logic and add logging.

--- database/messages.js
+++ database/messages.js
@@ -1,31 +1,15 @@
 const messages = new Map();
 
 function save(id, message) {
-
-    // Ignore delete notifications
-    if (message.message?.protocolMessage) return;
-
-    const data = {
-        id,
-        message,
-        timestamp: Date.now()
-    };
-
-    messages.set(id, data);
-
-    if (message.key?.id) {
-        messages.set(message.key.id, data);
-    }
-
-    if (messages.size > 1000) {
-        const firstKey = messages.keys().next().value;
-        messages.delete(firstKey);
+    if (!message.message?.protocolMessage) {
+        messages.set(id, message);
+        console.log("SAVED ID:", id);
     }
 }
 
 function get(id) {
-    const data = messages.get(id);
-    return data ? data.message : null;
+    console.log("SEARCH ID:", id);
+    return messages.get(id);
 }
 
 module.exports = {
