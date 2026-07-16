f15bd84aad7b8f3ce7167eacc530c14238591e38
mrdarkking632 <Adeel787403@gmail.com>
2026-07-16 15:36:33 +0500

    Enhance message saving and retrieval logic
    Refactor save and get functions to handle protocol messages and updates.

--- database/messages.js
+++ database/messages.js
@@ -1,15 +1,36 @@
 const messages = new Map();
 
 function save(id, message) {
-    if (!message.message?.protocolMessage) {
-        messages.set(id, message);
-        console.log("SAVED ID:", id);
+
+    if (message.message?.protocolMessage) return;
+
+    messages.set(id, message);
+
+    if (message.key?.remoteJidAlt) {
+        messages.set(
+            message.key.remoteJidAlt + id,
+            message
+        );
     }
+
+    console.log("SAVED ID:", id);
 }
 
-function get(id) {
-    console.log("SEARCH ID:", id);
-    return messages.get(id);
+function get(id, update) {
+
+    let msg = messages.get(id);
+
+    if (msg) return msg;
+
+    if (update?.key?.remoteJidAlt) {
+        for (const [key, value] of messages) {
+            if (key.includes(update.key.remoteJidAlt)) {
+                return value;
+            }
+        }
+    }
+
+    return null;
 }
 
 module.exports = {
