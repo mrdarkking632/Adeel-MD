869180f52b77d5d5d2424302fa1a458331ca45d8
mrdarkking632 <Adeel787403@gmail.com>
2026-07-16 15:42:17 +0500

    Update messages.js

--- database/messages.js
+++ database/messages.js
@@ -16,23 +16,9 @@ function save(id, message) {
     console.log("SAVED ID:", id);
 }
 
-function get(id, update) {
-
-    let msg = messages.get(id);
-
-    if (msg) return msg;
-
-    if (update?.key?.remoteJidAlt) {
-        for (const [key, value] of messages) {
-            if (key.includes(update.key.remoteJidAlt)) {
-                return value;
-            }
-        }
-    }
-
-    return null;
+function get(id) {
+    return messages.get(id);
 }
-
 module.exports = {
     save,
     get
